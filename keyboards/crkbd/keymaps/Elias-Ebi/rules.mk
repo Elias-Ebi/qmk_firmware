d1d517b0bc716ad5507a3a2c5f97aa124ac6908a
Elias-Ebi <elias.ebi@uni-ulm.de>
2023-01-14 18:09:21 +0100

    .

---
+++ keyboards/crkbd/keymaps/Elias-Ebi/rules.mk
@@ -0,0 +1,10 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+COMMAND_ENABLE 	= no
+TAP_DANCE_ENABLE = yes
+#CAPS_WORD_ENABLE = yes
+SEND_STRING_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
